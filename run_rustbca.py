Fixes to surface refraction. There was a sign error causing refraction to reflect particles headed down or left.

run_rustbca.py
@@ -23,14 +23,14 @@ def refraction(E, cosx, cosy, cosz, Es, dx, dy):
     costheta = (cosx*dx + cosy*dy)
     return costheta
 
-def main(Zb, Mb, n, Ec, Es, Eb, Ma, Za, E0, N, theta, thickness, depth, name='test_'):
+def main(Zb, Mb, n, Ec, Es, Eb, Ma, Za, E0, N, N_, theta, thickness, depth, track_trajectories=False, track_recoils=False, track_recoil_trajectories=False, write_files=True, name='test_'):
 
     options = {
         'name': name,
-        'track_trajectories': False,
-        'track_recoils': True,
-        'track_recoil_trajectories': False,
-        'write_files': True
+        'track_trajectories': track_trajectories,
+        'track_recoils': track_recoils,
+        'track_recoil_trajectories': track_recoil_trajectories,
+        'write_files': write_files
     }
 
     material_parameters = {
@@ -64,6 +64,7 @@ def main(Zb, Mb, n, Ec, Es, Eb, Ma, Za, E0, N, theta, thickness, depth, name='te
         'length_unit': 'MICRON',
         'energy_unit': 'EV',
         'mass_unit': 'AMU',
+        'N': [N_ for _ in range(N)],
         'm': [Ma for _ in range(N)],
         'Z': [Za for _ in range(N)],
         'E': [E0 for _ in range(N)],
@@ -81,8 +82,8 @@ def main(Zb, Mb, n, Ec, Es, Eb, Ma, Za, E0, N, theta, thickness, depth, name='te
     with open('input.toml', 'w') as file:
         toml.dump(input_file, file, encoder=toml.TomlNumpyEncoder())
 
-    os.system('rm *.output')
-    os.system('cargo run --release')
+    #os.system('rm *.output')
+    #os.system('cargo run --release')
 
     do_plots(surface, energy_surface, simulation_surface, name)
 
@@ -140,27 +141,35 @@ def do_plots(surface, energy_surface, simulation_surface, name):
         plt.scatter(sputtered[:,3], sputtered[:,4], s=50, color='blue', marker='*')
     plt.xlabel('x [um]')
     plt.ylabel('y [um]')
-    plt.title('Helium trajectories in tungsten')
+    plt.title('5 MeV Helium Deposition on Copper')
 
     if np.size(deposited) > 0:
         plt.figure(2)
-        num_bins = 50
-        bins = np.linspace(miny, maxy, num_bins)
-        plt.hist(deposited[:, 3], bins=bins)
-        plt.title('Helium y-Deposition')
+        num_bins = 200
+        bins = np.linspace(minx, maxx, num_bins)
+        plt.hist(deposited[:, 2], bins=bins)
+        plt.title('5 MeV Helium x-Deposition on Copper')
         plt.xlabel('y [um]')
         plt.ylabel('Helium Deposition (A.U.)')
 
         plt.figure(3)
-        num_bins_x = 50
-        num_bins_y = 50
+        num_bins = 200
+        bins = np.linspace(miny, maxy, num_bins)
+        plt.hist(deposited[:, 3], bins=bins)
+        plt.title('5 MeV Helium y-Deposition on Copper')
+        plt.xlabel('y [um]')
+        plt.ylabel('Helium Deposition (A.U.)')
+
+        plt.figure(4)
+        num_bins_x = 200
+        num_bins_y = 200
         binx = np.linspace(minx, maxx, num_bins_x)
         biny = np.linspace(miny, maxy, num_bins_y)
         plt.hist2d(deposited[:, 2], deposited[:, 3], bins=(binx, biny))
         plt.plot(*surface.exterior.xy, color='dimgray')
         plt.plot(*energy_surface.exterior.xy, '--', color='dimgray')
         plt.plot(*simulation_surface.exterior.xy, '--', color='dimgray')
-        plt.title('Helium Deposition')
+        plt.title('5 MeV Helium Deposition on Copper')
         plt.xlabel('x [um]')
         plt.ylabel('y [um]')
         #plt.axis('square')
@@ -176,9 +185,10 @@ if __name__ == '__main__':
     Eb = 1.0
     Ma = 4
     Za = 2
-    E0 = 1E6
-    N = 10000
+    E0 = 5E6
+    N = 100000
+    N_ = 1
     theta = 0.00001
-    thickness = 10
-    depth = 10
-    main(Zb, Mb, n, Ec, Es, Eb, Ma, Za, E0, N, theta, thickness, depth)
+    thickness = 1
+    depth = 20
+    main(Zb, Mb, n, Ec, Es, Eb, Ma, Za, E0, N, N_, theta, thickness, depth, track_trajectories=False, track_recoils=False, track_recoil_trajectories=False, write_files=True)