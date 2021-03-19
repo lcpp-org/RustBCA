da76aba5e86e7a6544363c08164bb7db93b5ae89
Jon Drobny <drobny2@illinois.edu>
2021-03-19 16:57:33 -0500

    update

---
+++ RustBCA.h
@@ -0,0 +1,87 @@
+#include <cstdarg>
+#include <cstdint>
+#include <cstdlib>
+#include <ostream>
+#include <new>
+#include <cmath>
+
+static const double PI = M_PI;
+
+static const double FRAC_2_SQRT_PI = M_2_PI;
+
+static const double SQRT_2 = M_SQRT2;
+
+///Fundamental charge in Coulombs.
+static const double Q = 1.602176634e-19;
+
+/// One electron-volt in Joules.
+static const double EV = Q;
+
+/// One atomic mass unit in kilograms.
+static const double AMU = 1.66053906660e-27;
+
+/// One Angstrom in meters.
+static const double ANGSTROM = 1e-10;
+
+/// One micron in meters.
+static const double MICRON = 1e-6;
+
+/// One nanometer in meters.
+static const double NM = 1e-9;
+
+/// One centimeter in meters.
+static const double CM = 1e-2;
+
+/// Vacuum permitivity in Farads/meter.
+static const double EPS0 = 8.8541878128e-12;
+
+/// Bohr radius in meters.
+static const double A0 = 5.29177210903e-11;
+
+/// Electron mass in kilograms.
+static const double ME = 9.1093837015e-31;
+
+/// sqrt(pi).
+static const double SQRTPI = (2. / FRAC_2_SQRT_PI);
+
+/// sqrt(2 * pi).
+static const double SQRT2PI = ((2. * SQRT_2) / FRAC_2_SQRT_PI);
+
+/// Speed of light in meters/second.
+static const double C = 299792458.;
+
+/// Bethe-Bloch electronic stopping prefactor, in SI units.
+static const double BETHE_BLOCH_PREFACTOR = ((((((4. * PI) * ((Q * Q) / ((4. * PI) * EPS0))) * ((Q * Q) / ((4. * PI) * EPS0))) / ME) / C) / C);
+
+/// Lindhard-Scharff electronic stopping prefactor, in SI units.
+static const double LINDHARD_SCHARFF_PREFACTOR = (((1.212 * ANGSTROM) * ANGSTROM) * Q);
+
+/// Lindhard reduced energy prefactor, in SI units.
+static const double LINDHARD_REDUCED_ENERGY_PREFACTOR = ((((4. * PI) * EPS0) / Q) / Q);
+
+struct OutputBCA {
+  uintptr_t len;
+  double (*particles)[9];
+};
+
+extern "C" {
+
+OutputBCA simple_bca_c(double x,
+                       double y,
+                       double z,
+                       double ux,
+                       double uy,
+                       double uz,
+                       double E1,
+                       double Z1,
+                       double m1,
+                       double Ec1,
+                       double Es1,
+                       double Z2,
+                       double m2,
+                       double Ec2,
+                       double Es2,
+                       double n2,
+                       double Eb2);
+
+} // extern "C"
---
+++ src/lib.rs
@@ -0,0 +1,184 @@
+#![allow(unused_variables)]
+#![allow(non_snake_case)]
+#![allow(non_camel_case_types)]
+
+#[cfg(feature = "cpr_rootfinder_openblas")]
+extern crate openblas_src;
+#[cfg(feature = "cpr_rootfinder_netlib")]
+extern crate netlib_src;
+#[cfg(feature = "cpr_rootfinder_intel_mkl")]
+extern crate intel_mkl_src;
+
+use std::{fmt};
+use std::mem::discriminant;
+
+//Error handling crate
+use anyhow::Result;
+use anyhow::*;
+
+//Serializing/Deserializing crate
+use serde::*;
+
+//Array input via hdf5
+#[cfg(feature = "hdf5_input")]
+use hdf5::*;
+
+//I/O
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+use std::io::BufWriter;
+
+//itertools
+use itertools::izip;
+
+//Math
+use std::f64::consts::FRAC_2_SQRT_PI;
+use std::f64::consts::PI;
+use std::f64::consts::SQRT_2;
+
+use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
+
+//Load internal modules
+pub mod material;
+pub mod particle;
+pub mod tests;
+pub mod interactions;
+pub mod bca;
+pub mod geometry;
+pub mod input;
+pub mod output;
+pub mod enums;
+pub mod consts;
+pub mod structs;
+pub mod sphere;
+
+#[cfg(feature = "parry3d")]
+pub mod parry;
+
+pub use crate::enums::*;
+pub use crate::consts::*;
+pub use crate::structs::*;
+pub use crate::input::{Input2D, Input1D, Input0D, Options, InputFile, GeometryInput};
+pub use crate::output::{OutputUnits};
+pub use crate::geometry::{Geometry, GeometryElement, Mesh0D, Mesh1D, Mesh2D};
+pub use crate::sphere::{Sphere, SphereInput, InputSphere};
+
+#[cfg(feature = "parry3d")]
+pub use crate::parry::{ParryBall, ParryBallInput, InputParryBall, ParryTriMesh, ParryTriMeshInput, InputParryTriMesh};
+
+#[pymodule]
+pub fn pybca(py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(simple_bca_py, m)?)?;
+    Ok(())
+}
+
+#[repr(C)]
+pub struct OutputBCA {
+    len: usize,
+    pub particles: *mut [f64; 9],
+}
+
+#[no_mangle]
+pub extern "C" fn simple_bca_c(x: f64, y: f64, z: f64, ux: f64, uy: f64, uz: f64, E1: f64, Z1: f64, m1: f64, Ec1: f64, Es1: f64, Z2: f64, m2: f64, Ec2: f64, Es2: f64, n2: f64, Eb2: f64) -> OutputBCA {
+    let mut output = simple_bca(x, y, z, ux, uy, uz, E1, Z1, m1, Ec1, Es1, Z2, m2, Ec2, Es2, n2, Eb2);
+    let len = output.len();
+    let particles = output.as_mut_ptr();
+
+    std::mem::forget(output);
+    OutputBCA {
+        len,
+        particles
+    }
+}
+
+#[pyfunction]
+pub fn simple_bca_py(x: f64, y: f64, z: f64, ux: f64, uy: f64, uz: f64, E1: f64, Z1: f64, m1: f64, Ec1: f64, Es1: f64, Z2: f64, m2: f64, Ec2: f64, Es2: f64, n2: f64, Eb2: f64) -> Vec<[f64; 9]> {
+    simple_bca(x, y, z, ux, uy, uz, E1, Z1, m1, Ec1, Es1, Z2, m2, Ec2, Es2, n2, Eb2)
+}
+
+pub fn simple_bca(x: f64, y: f64, z: f64, ux: f64, uy: f64, uz: f64, E1: f64, Z1: f64, m1: f64, Ec1: f64, Es1: f64, Z2: f64, m2: f64, Ec2: f64, Es2: f64, n2: f64, Eb2: f64) -> Vec<[f64; 9]> {
+
+    let options = Options {
+        name: "test".to_string(),
+        track_trajectories: false,
+        track_recoils: true,
+        track_recoil_trajectories: false,
+        write_buffer_size: 8000,
+        weak_collision_order: 3,
+        suppress_deep_recoils: false,
+        high_energy_free_flight_paths: false,
+        electronic_stopping_mode: ElectronicStoppingMode::INTERPOLATED,
+        mean_free_path_model: MeanFreePathModel::LIQUID,
+        interaction_potential: vec![vec![InteractionPotential::KR_C]],
+        scattering_integral: vec![vec![ScatteringIntegral::MENDENHALL_WELLER]],
+        num_threads: 1,
+        num_chunks: 1,
+        use_hdf5: false,
+        root_finder: vec![vec![Rootfinder::DEFAULTNEWTON]],
+        track_displacements: false,
+        track_energy_losses: false,
+    };
+
+    let p = particle::Particle {
+        m: m1*AMU,
+        Z: Z1,
+        E: E1*EV,
+        Ec: Ec1*EV,
+        Es: Es1*EV,
+        pos: Vector::new(x, y, z),
+        dir: Vector::new(ux, uy, uz),
+        pos_origin: Vector::new(x, y, z),
+        pos_old: Vector::new(x, y, z),
+        dir_old: Vector::new(ux, uy, uz),
+        energy_origin: E1*EV,
+        asymptotic_deflection: 0.0,
+        stopped: false,
+        left: false,
+        incident: true,
+        first_step: true,
+        trajectory: vec![],
+        energies: vec![],
+        track_trajectories: false,
+        number_collision_events: 0,
+        backreflected: false,
+        interaction_index : 0
+    };
+
+    let material_parameters = material::MaterialParameters {
+        energy_unit: "EV".to_string(),
+        mass_unit: "AMU".to_string(),
+        Eb: vec![Eb2],
+        Es: vec![Es2],
+        Ec: vec![Ec2],
+        Z: vec![Z2],
+        m: vec![m2],
+        interaction_index: vec![0],
+        surface_binding_model: SurfaceBindingModel::AVERAGE,
+        bulk_binding_model: BulkBindingModel::INDIVIDUAL,
+    };
+
+    let geometry_input = geometry::Mesh0DInput {
+        length_unit: "ANGSTROM".to_string(),
+        densities: vec![n2],
+        electronic_stopping_correction_factor: 1.0
+    };
+
+    let m = material::Material::<Mesh0D>::new(&material_parameters, &geometry_input);
+
+    let output = bca::single_ion_bca(p, &m, &options);
+
+    output.iter().filter(|particle| (particle.incident) | (particle.left)).map(|particle|
+        [
+            particle.Z,
+            particle.m/AMU,
+            particle.E/EV,
+            particle.pos.x/ANGSTROM,
+            particle.pos.y/ANGSTROM,
+            particle.pos.z/ANGSTROM,
+            particle.dir.x,
+            particle.dir.y,
+            particle.dir.z
+        ]
+    ).collect()
+}
