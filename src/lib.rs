866c3f811208ec3070979255a5263cc61bd8fae9
Jon Drobny <drobny2@illinois.edu>
2021-07-22 15:22:44 -0700

    Compound bindings added, use with compound_list_bca_c

--- src/lib.rs
+++ src/lib.rs
@@ -101,6 +101,25 @@ pub struct InputSimpleBCA {
     pub Eb2: f64,
 }
 
+#[derive(Debug)]
+#[repr(C)]
+pub struct InputCompoundBCA {
+    pub len: usize,
+    /// vx, vy, vz
+    pub velocities: *mut [f64; 3],
+    pub Z1: f64,
+    pub m1: f64,
+    pub Ec1: f64,
+    pub Es1: f64,
+    pub num_species_target: usize,
+    pub Z2: *mut f64,
+    pub m2: *mut f64,
+    pub n2: *mut f64,
+    pub Ec2: *mut f64,
+    pub Es2: *mut f64,
+    pub Eb2: *mut f64,
+}
+
 #[no_mangle]
 pub extern "C" fn simple_bca_list_c(input: InputSimpleBCA) -> OutputBCA {
 
@@ -185,6 +204,176 @@ pub extern "C" fn simple_bca_list_c(input: InputSimpleBCA) -> OutputBCA {
     let geometry_input = geometry::Mesh0DInput {
         length_unit: "ANGSTROM".to_string(),
         densities: vec![input.n2],
+        electronic_stopping_correction_factor: 1.0
+    };
+
+    let m = material::Material::<Mesh0D>::new(&material_parameters, &geometry_input);
+
+    let velocities = unsafe { slice::from_raw_parts(input.velocities, input.len) };
+
+    for velocity in velocities {
+
+        let vx = velocity[0];
+        let vy = velocity[1];
+        let vz = velocity[2];
+
+        let v = (vx*vx + vy*vy + vz*vz).sqrt();
+
+        let E1 = 0.5*input.m1*AMU*v*v;
+
+        let ux = vx/v;
+        let uy = vy/v;
+        let uz = vz/v;
+
+        let p = particle::Particle {
+            m: input.m1*AMU,
+            Z: input.Z1,
+            E: E1,
+            Ec: input.Ec1*EV,
+            Es: input.Es1*EV,
+            pos: Vector::new(x, y, z),
+            dir: Vector::new(ux, uy, uz),
+            pos_origin: Vector::new(x, y, z),
+            pos_old: Vector::new(x, y, z),
+            dir_old: Vector::new(ux, uy, uz),
+            energy_origin: E1,
+            asymptotic_deflection: 0.0,
+            stopped: false,
+            left: false,
+            incident: true,
+            first_step: true,
+            trajectory: vec![],
+            energies: vec![],
+            track_trajectories: false,
+            number_collision_events: 0,
+            backreflected: false,
+            interaction_index : 0
+        };
+
+
+        let output = bca::single_ion_bca(p, &m, &options);
+
+        for particle in output {
+
+            if (particle.left) | (particle.incident) {
+                total_output.push(
+                    [
+                        particle.Z,
+                        particle.m/AMU,
+                        particle.E/EV,
+                        particle.pos.x/ANGSTROM,
+                        particle.pos.y/ANGSTROM,
+                        particle.pos.z/ANGSTROM,
+                        particle.dir.x,
+                        particle.dir.y,
+                        particle.dir.z
+                    ]
+                );
+            }
+        }
+    }
+
+    let len = total_output.len();
+    let particles = total_output.as_mut_ptr();
+
+    std::mem::forget(total_output);
+    OutputBCA {
+        len,
+        particles
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn compound_bca_list_c(input: InputCompoundBCA) -> OutputBCA {
+
+    let mut total_output = vec![];
+
+    #[cfg(feature = "distributions")]
+    let options = Options {
+        name: "test".to_string(),
+        track_trajectories: false,
+        track_recoils: true,
+        track_recoil_trajectories: false,
+        write_buffer_size: 8000,
+        weak_collision_order: 3,
+        suppress_deep_recoils: false,
+        high_energy_free_flight_paths: false,
+        electronic_stopping_mode: ElectronicStoppingMode::INTERPOLATED,
+        mean_free_path_model: MeanFreePathModel::LIQUID,
+        interaction_potential: vec![vec![InteractionPotential::KR_C]],
+        scattering_integral: vec![vec![ScatteringIntegral::MENDENHALL_WELLER]],
+        num_threads: 1,
+        num_chunks: 1,
+        use_hdf5: false,
+        root_finder: vec![vec![Rootfinder::DEFAULTNEWTON]],
+        track_displacements: false,
+        track_energy_losses: false,
+        energy_min: 0.0,
+        energy_max: 10.0,
+        energy_num: 11,
+        angle_min: 0.0,
+        angle_max: 90.0,
+        angle_num: 11,
+        x_min: 0.0,
+        y_min: -10.0,
+        z_min: -10.0,
+        x_max: 10.0,
+        y_max: 10.0,
+        z_max: 10.0,
+        x_num: 11,
+        y_num: 11,
+        z_num: 11,
+    };
+
+    #[cfg(not(feature = "distributions"))]
+    let options = Options {
+        name: "test".to_string(),
+        track_trajectories: false,
+        track_recoils: true,
+        track_recoil_trajectories: false,
+        write_buffer_size: 8000,
+        weak_collision_order: 3,
+        suppress_deep_recoils: false,
+        high_energy_free_flight_paths: false,
+        electronic_stopping_mode: ElectronicStoppingMode::INTERPOLATED,
+        mean_free_path_model: MeanFreePathModel::LIQUID,
+        interaction_potential: vec![vec![InteractionPotential::KR_C]],
+        scattering_integral: vec![vec![ScatteringIntegral::MENDENHALL_WELLER]],
+        num_threads: 1,
+        num_chunks: 1,
+        use_hdf5: false,
+        root_finder: vec![vec![Rootfinder::DEFAULTNEWTON]],
+        track_displacements: false,
+        track_energy_losses: false,
+    };
+
+    let Z2 = unsafe { slice::from_raw_parts(input.Z2, input.num_species_target).to_vec() };
+    let m2 = unsafe { slice::from_raw_parts(input.m2, input.num_species_target).to_vec() };
+    let n2 = unsafe { slice::from_raw_parts(input.n2, input.num_species_target).to_vec() };
+    let Ec2 = unsafe { slice::from_raw_parts(input.Ec2, input.num_species_target).to_vec() };
+    let Es2 = unsafe { slice::from_raw_parts(input.Es2, input.num_species_target).to_vec() };
+    let Eb2 = unsafe { slice::from_raw_parts(input.Eb2, input.num_species_target).to_vec() };
+
+    let x = -2.*(n2.iter().sum::<f64>()*10E30).powf(-1./3.);
+    let y = 0.0;
+    let z = 0.0;
+
+    let material_parameters = material::MaterialParameters {
+        energy_unit: "EV".to_string(),
+        mass_unit: "AMU".to_string(),
+        Eb: Eb2,
+        Es: Es2,
+        Ec: Ec2,
+        Z: Z2,
+        m: m2,
+        interaction_index: vec![0],
+        surface_binding_model: SurfaceBindingModel::AVERAGE,
+        bulk_binding_model: BulkBindingModel::INDIVIDUAL,
+    };
+
+    let geometry_input = geometry::Mesh0DInput {
+        length_unit: "ANGSTROM".to_string(),
+        densities: n2,
         electronic_stopping_correction_factor: 1.0
     };
 
