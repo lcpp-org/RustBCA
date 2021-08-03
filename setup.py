8a4d96e767fe07c39dbee1b224afbc2ba9c83794
Jon Drobny <drobny2@illinois.edu>
2021-08-03 12:15:14 -0700

    Updated setup.py to ensure pyo3 is built.

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ setup(
     name="RustBCA",
     rust_extensions=[RustExtension("libRustBCA.pybca", binding=Binding.PyO3)],
     # rust extensions are not zip safe, just like C-extensions.
+    features=["python"]
     zip_safe=False,
 )
