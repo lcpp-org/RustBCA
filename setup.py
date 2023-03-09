f5c2858bee7bd33c8751d4f34e62ea1b82084afe
Jon Drobny <jdrobny@tae.com>
2023-03-09 09:15:45 -0800

    Update version numbers.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools_rust import Binding, RustExtension
 
 setup(
     name="RustBCA",
-    version="1.3.0",
+    version="2.0.0",
     rust_extensions=[
         RustExtension(
             "libRustBCA",
