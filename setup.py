63a032b38ef336bc48165cd9e4002646d368ecac
Jon Drobny <37962344+drobnyjt@users.noreply.github.com>
2023-06-07 09:03:11 -0700

    Update setup.py version number

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools_rust import Binding, RustExtension
 
 setup(
     name="RustBCA",
-    version="2.0.0",
+    version="2.1.0",
     rust_extensions=[
         RustExtension(
             "libRustBCA",
             binding=Binding.PyO3,
             features=["python", "parry3d"],
-            #args=["+nightly", "--edition 2018", "-Z unstable-options"],
+            #args=["+nightly", "--edition 2021", "-Z unstable-options"],
             #optional=True,
             #rust_version="1.57.0"
         )
