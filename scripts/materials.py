f088b7d236f9840e66bf1d19e32794f6cfedd955
Jon Drobny <contact@jondrobny.com>
2026-07-28 16:28:12 -0700

    Add warning to materials.py

--- scripts/materials.py
+++ scripts/materials.py
@@ -1,3 +1,7 @@
+import sys, os
+if os.environ.get("SUPPRESS_RUSTBCA_MATERIALS_WARNING") != "1":
+    print('materials.py is intended to provide example input values only; all input values should be verified by the user before running.', file=sys.stderr)
+
 titanium = {
     'symbol': 'Ti',
     'name': 'titanium',
