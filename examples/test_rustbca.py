Fixes to matrix algebra for rotation.

examples/test_rustbca.py
@@ -27,15 +27,21 @@ def main():
     uz = 0.0
 
     print(f'Before rotation: ({ux}, {uy}, {uz})')
-    ux, uy, uz = rotate_given_surface_normal_py(nx, ny, nz, ux, uy, uz)
-    print(f'After rotation: ({ux}, {uy}, {uz})')
-    ux, uy, uz = rotate_back_py(nx, ny, nz, ux, uy, uz)
-    print(f'After rotation back: ({ux}, {uy}, {uz})')
+    ux1, uy1, uz1 = rotate_given_surface_normal_py(nx, ny, nz, ux, uy, uz)
+    print(f'After rotation: ({ux1}, {uy1}, {uz1})')
+    ux2, uy2, uz2 = rotate_back_py(nx, ny, nz, ux1, uy1, uz1)
+    print(f'After rotation back: ({ux2}, {uy2}, {uz2})')
 
     #After rotating and rotating back, effect should be where you started (minus fp error)
-    assert(abs(ux - 1.0) < 1e-6)
-    assert(abs(uy) < 1e-6)
-    assert(abs(uz) < 1e-6)
+    np.testing.assert_allclose(ux2 - 1.0, 0.0)
+    np.testing.assert_allclose(uy2, 0.0, atol=1e-9)
+    np.testing.assert_allclose(uz2, 0.0, atol=1e-9)
+
+
+    #If ux is (1, 0, 0), this transform should just swap u with -n
+    #np.testing.assert_approx_equal(ux1, -nx)
+    #np.testing.assert_approx_equal(uy1, -ny)
+    #np.testing.assert_approx_equal(uz1, -nz)
 
     #test vectorized rotation to and from RustBCA coordinates
 
@@ -46,21 +52,46 @@ def main():
     ny = [-np.sqrt(2)/2]*num_rot_test
     nz = [0.0]*num_rot_test
 
+    np.random.seed(0)
+    nx = np.random.uniform(-1.0, 0.0, num_rot_test)
+    ny = np.random.uniform(-1.0, 1.0, num_rot_test)
+    nz = np.random.uniform(-1.0, 1.0, num_rot_test)
+    mags = np.sqrt(nx*nx + ny*ny + nz*nz)
+    nx /= mags
+    ny /= mags
+    nz /= mags
+
     #ux, uy, uz is the particle direction (simulation coordinates)
-    ux = [1.0]*num_rot_test
-    uy = [0.0]*num_rot_test
-    uz = [0.0]*num_rot_test
+    np.random.seed(0)
+    ux = np.random.uniform(-1.0, 1.0, num_rot_test)
+    uy = np.random.uniform(-1.0, 1.0, num_rot_test)
+    uz = np.random.uniform(-1.0, 1.0, num_rot_test)
+    mags = np.sqrt(ux*ux + uy*uy + uz*uz)
+    ux /= mags
+    uy /= mags
+    uz /= mags
 
     start = time.time()
-    ux, uy, uz = rotate_given_surface_normal_vec_py(nx, ny, nz, ux, uy, uz)
-    ux, uy, uz = rotate_back_vec_py(nx, ny, nz, ux, uy, uz)
+    ux1, uy1, uz1 = rotate_given_surface_normal_vec_py(nx, ny, nz, ux, uy, uz)
+    ux2, uy2, uz2 = rotate_back_vec_py(nx, ny, nz, ux1, uy1, uz1)
     stop = time.time()
     print(f'Time to rotate: {(stop - start)/num_rot_test} sec/vector')
 
+    # ensure that R n = (-1, 0, 0)
+    nx1, ny1, nz1 = rotate_given_surface_normal_vec_py(nx, ny, nz, nx, ny, nz)
+    np.testing.assert_allclose(nx1, -1.0)
+    np.testing.assert_allclose(ny1, 0.0, atol=1e-12)
+    np.testing.assert_allclose(nz1, 0.0, atol=1e-12)
+
+    # ensure angle between n and u remains constant
+    cosine = nx*ux + ny*uy + nz*uz
+    cosine1 = -np.array(ux1) # here n becomes (-1, 0, 0)
+    np.testing.assert_allclose(cosine, cosine1)
+
     #After rotating and rotating back, effect should be where you started (minus fp error)
-    assert(abs(ux[0] - 1.0) < 1e-6)
-    assert(abs(uy[0]) < 1e-6)
-    assert(abs(uz[0]) < 1e-6)
+    np.testing.assert_allclose(ux, ux2)
+    np.testing.assert_allclose(uy, uy2)
+    np.testing.assert_allclose(uz, uz2)
 
     #scripts/materials.py has a number of potential ions and targets
     ion = helium
@@ -92,7 +123,6 @@ def main():
     np.testing.assert_approx_equal(R_N, 0.424)
     np.testing.assert_approx_equal(R_E, 0.22840032456593984)
 
-
     vx0 = 1e5
     vy0 = 1e5
     vz0 = 0.0
@@ -102,7 +132,7 @@ def main():
     print(f'(vx, vy, vz) after reflection: ({vx1}, {vy1}, {vz1})')
 
     #For smooth distributions and good statistics, you should use at least 10k ions
-    number_ions = 10000
+    number_ions = 100000
 
     #1 keV is above the He on W sputtering threshold of ~150 eV
     energies_eV = 1000.0*np.ones(number_ions)
@@ -171,6 +201,8 @@ def main():
     # check that reflection/sputtering are reasonable
     assert(0.01 < len(sputtered[:, 0])/number_ions < 0.03)
     assert(0.4 < len(reflected[:, 0])/number_ions < 0.6)
+
+    number_ions = 10000
 
     #Next up is the layered target version. I'll add a 50 Angstrom layer of W-H to the top of the target.
 