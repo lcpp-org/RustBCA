b93772facf187581a88a8e46ece57a9f1e337d22
Jon Drobny <drobny2@illinois.edu>
2026-07-31 13:59:49 -0700

    Somehow branchless duff got overwritten - fixed. Removed warning from tests about mutable vector.

--- src/math.rs
+++ src/math.rs
@@ -4,17 +4,10 @@ use super::*;
 /// Duff et al., JCGT 2017
 /// http://jcgt.org/published/0006/01/01/
 pub fn duff_orthonormal_basis(n: Vector) -> (Vector, Vector) {
-    if n.z < 0. {
-        let a = 1.0 / (1.0 - n.z);
-        let b = n.x * n.y * a;
-        let b1 = Vector::new(1.0 - n.x*n.x*a, -b, n.x);
-        let b2 = Vector::new(b, n.y * n.y*a- 1.0, -n.y);
-        (b1, b2)
-    } else {
-        let a = 1.0 / (1.0 + n.z);
-        let b = -n.x * n.y * a;
-        let b1 = Vector::new(1.0 - n.x*n.x*a, b, -n.x);
-        let b2 = Vector::new(b, 1.0 - n.y*n.y*a, -n.y);
-        (b1, b2)
-    }
+    let sign = (1.0_f64).copysign(n.z);
+    let a = -1.0_f64 / (sign + n.z);
+    let b = n.x*n.y*a;
+    let b1 = Vector::new(1.0 + sign*n.x*n.x*a, sign*b, -sign*n.x);
+    let b2 = Vector::new(b, sign + n.y*n.y*a, -n.y);
+    (b1, b2)
 }
