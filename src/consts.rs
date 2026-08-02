251dbb6078449266d89d6cc17034e7c360e31b4d
Jon Drobny <drobny2@illinois.edu>
2026-08-02 14:12:55 -0700

    Fix to cfg statement re Z_MAX

--- src/consts.rs
+++ src/consts.rs
@@ -38,7 +38,7 @@ pub const LINDHARD_REDUCED_ENERGY_PREFACTOR: f64 = 4.*PI*EPS0/Q/Q;
 /// Maximum atomic number in RustBCA.
 #[cfg(not(feature = "extended_max_z"))]
 pub const Z_MAX: usize = 92;
-#[cfg(feature(="extended_max_z"))]
+#[cfg(feature="extended_max_z")]
 pub const Z_MAX: usize = 120;
 //https://math.stackexchange.com/questions/2388887/
 //num elements in a triangular NxN matrix (including diag)
