8b2e34cc83ee61faaebd70d0edb57cdc2746e4da
Jon Drobny <drobny2@illinois.edu>
2026-08-03 15:19:28 -0700

    Undo trilut

--- src/interactions.rs
+++ src/interactions.rs
@@ -261,7 +261,9 @@ pub fn screened_coulomb(r: f64, a: f64, Za: f64, Zb: f64, interaction_potential:
 
 /// Coulombic interaction potential.
 pub fn coulomb(r: f64, Za: f64, Zb: f64) -> f64 {
-    COULOMB_CONSTANT_TABLE[Za as usize * Z_MAX + Zb as usize]/r
+    let mut i = Za as usize;
+    let mut j = Zb as usize;
+    COULOMB_CONSTANT_TABLE[triangular_index(&mut i, &mut j)]/r
 }
 
 /// Screening functions for screened-coulomb interaction potentials.
@@ -306,29 +308,32 @@ pub fn screening_length(Za: f64, Zb: f64, interaction_potential: InteractionPote
 // It turns out it's faster (~10% speedup) to just generate every possible screening length as a lookup table
 // LazyLock is a thread-safe value that is initialized whenever it is first accessed
 // It will block other threads while it runs, but it should run extremely quickly and only once
-static LINDHARD_SCREENING_LENGTH_TABLE: LazyLock<[f64; Z_MAX*Z_MAX]> = LazyLock::new(
+static LINDHARD_SCREENING_LENGTH_TABLE: LazyLock<[f64; TABLE_SIZE]> = LazyLock::new(
     ||
-    std::array::from_fn(
-        |i| {
-            // standard 2D to 1D  array indexing
-            // I always have to look this up; copied from here
-            // (e.g. https://stackoverflow.com/questions/5494974/convert-1d-array-index-to-2d-array-index)
-            let Za = i / Z_MAX;
-            let Zb = i % Z_MAX;
-            lindhard_screening_length(Za as f64, Zb as f64)
+    {
+        let mut array = [0.0; TABLE_SIZE];
+        for i in 0..Z_MAX {
+            for j in 0..=i {
+                let index = (i * (i + 1))/2 + j;
+                array[index] = lindhard_screening_length(i as f64, j as f64);
+            }
         }
-    )
+        array
+    }
 );
 
-static ZBL_SCREENING_LENGTH_TABLE: LazyLock<[f64; Z_MAX*Z_MAX]> = LazyLock::new(
+static ZBL_SCREENING_LENGTH_TABLE: LazyLock<[f64; TABLE_SIZE]> = LazyLock::new(
     ||
-    std::array::from_fn(
-        |i| {
-            let Za = i / Z_MAX;
-            let Zb = i % Z_MAX;
-            zbl_screening_length(Za as f64, Zb as f64)
+    {
+        let mut array = [0.0; TABLE_SIZE];
+        for i in 0..Z_MAX {
+            for j in 0..=i {
+                let index = (i * (i + 1))/2 + j;
+                array[index] = zbl_screening_length(i as f64, j as f64);
+            }
         }
-    )
+        array
+    }
 );
 
 pub fn zbl_screening_length(Za: f64, Zb: f64) -> f64{
@@ -341,12 +346,18 @@ pub fn lindhard_screening_length(Za: f64, Zb: f64) -> f64 {
 
 #[inline]
 pub fn lindhard_screening_length_lookup(Za: u64, Zb: u64) -> f64 {
-    LINDHARD_SCREENING_LENGTH_TABLE[Za as usize * Z_MAX + Zb as usize]
+    let mut i = Za as usize;
+    let mut j = Zb as usize;
+    
+    LINDHARD_SCREENING_LENGTH_TABLE[triangular_index(&mut i, &mut j)]
 }
 
 #[inline]
 pub fn zbl_screening_length_lookup(Za: u64, Zb: u64) -> f64{
-    ZBL_SCREENING_LENGTH_TABLE[Za as usize * Z_MAX + Zb as usize]
+    let mut i = Za as usize;
+    let mut j = Zb as usize;
+
+    ZBL_SCREENING_LENGTH_TABLE[triangular_index(&mut i, &mut j)]
 }
 
 /// Coefficients of inverse-polynomial interaction potentials.
