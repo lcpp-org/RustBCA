adcbab1bd0da209355b904a8122242bb405ac05c
Jon Drobny <drobny2@illinois.edu>
2026-08-11 10:42:54 -0700

    Remove more unwraps.

--- src/lib.rs
+++ src/lib.rs
@@ -150,9 +150,10 @@ mod libRustBCA {
 macro_rules! geometry_typed_silent_loops {
     ($geometry_type:ty, $input:expr, $python:expr) => {
         {
-            let input: <$geometry_type as geometry::Geometry>::InputFileFormat = depythonize(&$input).unwrap();
+            let input: <$geometry_type as geometry::Geometry>::InputFileFormat = depythonize(&$input)?;
             let (particle_input_array, material, options, output_units) = input::process_input_file(input);
-            let pool = rayon::ThreadPoolBuilder::new().num_threads(options.num_threads).build().unwrap();
+            let pool = rayon::ThreadPoolBuilder::new().num_threads(options.num_threads).build()
+                .map_err(|e| PyRuntimeError::new_err(format!("Failed to initialize thread pool.")))?;
             let finished_particles = pool.install( ||
                 physics::silent_physics_loop::<$geometry_type>(particle_input_array, material, options, output_units.clone())
             );
