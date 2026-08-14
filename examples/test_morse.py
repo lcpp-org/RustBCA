b4dbd254fddca7af02005e396503b9b0b1c424f0
Jon Drobny <drobny2@illinois.edu>
2026-08-14 15:33:06 -0700

    Further tweaks to ensure test_morse.py still works.

--- examples/test_morse.py
+++ examples/test_morse.py
@@ -25,7 +25,7 @@ def run_morse_potential(energy, index, num_samples=10000, run_sim=True):
     mean_free_path_model = "LIQUID"
     interaction_potential = [[{{"MORSE"={{D=5.4971E-20, r0=2.782E-10, alpha=1.4198E10}}}}]]
     scattering_integral = [["GAUSS_LEGENDRE"]]
-    root_finder = [[{{"CPR"={{n0=2, nmax=100, epsilon=1E-9, complex_threshold=1E-3, truncation_threshold=1E-9, far_from_zero=1E9, interval_limit=1E-12, derivative_free=true}}}}]]
+    root_finder = [[{{"CPR"={{n0=3, nmax=100, epsilon=1E-9, complex_threshold=1E-9, truncation_threshold=1E-9, far_from_zero=1E9, interval_limit=1E-13, derivative_free=true}}}}]]
     num_threads = 4
     num_chunks = 10
 
@@ -87,9 +87,9 @@ def run_krc_morse_potential(energy, index, num_samples=10000, run_sim=True):
     mean_free_path_model = "LIQUID"
     interaction_potential = [[{{"KRC_MORSE"={{D=5.4971E-20, r0=2.782E-10, alpha=1.4198E10, k=7E10, x0=0.75E-10}}}}]]
     scattering_integral = [["GAUSS_LEGENDRE"]]
-    root_finder = [[{{"CPR"={{n0=2, nmax=100, epsilon=1E-3, complex_threshold=1E-3, truncation_threshold=1E-9, far_from_zero=1E9, interval_limit=1E-12, derivative_free=true}}}}]]
-    num_threads = 4
-    num_chunks = 10
+    root_finder = [[{{"CPR"={{n0=2, nmax=200, epsilon=1E-9, complex_threshold=1E-9, truncation_threshold=1E-9, far_from_zero=1E9, interval_limit=1E-13, derivative_free=true}}}}]]
+    num_threads = 6
+    num_chunks = 1
 
     [particle_parameters]
     length_unit = "ANGSTROM"
@@ -172,8 +172,8 @@ plt.semilogx(energies, r_benchmark, marker='^', linestyle='', label='Exp.')
 #Running and plotting the H-Ni simulations with the Morse potential and updated Es
 num_energies = 15
 energies = np.logspace(-1, 4, num_energies)
-run_sim = False
-num_samples = 10000
+run_sim = True
+num_samples = 1000
 R_N = np.zeros(num_energies)
 R_E = np.zeros(num_energies)
 R_N_2 = np.zeros(num_energies)
