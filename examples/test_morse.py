29e63cfb547069dbcf6ea49de3005925fbe36c6f
Jon Drobny <drobny2@illinois.edu>
2026-08-19 22:00:19 -0700

    Left run_sim off in test_morse. fixed.

--- examples/test_morse.py
+++ examples/test_morse.py
@@ -176,7 +176,7 @@ plt.semilogx(energies, r_benchmark, marker='^', linestyle='', label='Exp.')
 #Running and plotting the H-Ni simulations with the Morse potential and updated Es
 num_energies = 20
 energies = np.logspace(-1, 4, num_energies)
-run_sim = False
+run_sim = True
 num_samples = 1000
 R_N = np.zeros(num_energies)
 R_E = np.zeros(num_energies)
